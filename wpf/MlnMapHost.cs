42af49d6fd2263a5f8f9fad6bde6031426494a5d
acalcutt <acalcutt@techidiots.net>
2026-05-29 11:32:00 -0400

    try to fix build errors

--- wpf/MlnMapHost.cs
+++ wpf/MlnMapHost.cs
@@ -14,6 +14,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
